0d509f7fa141044e2e064b11e6d3950988e7bf1c
Robert Crida <rcrida@gmail.com>
2026-04-15 12:12:05 +0100

    Drop the TreeSolver interface and rename TreeSolverImpl to TreeSolver

--- src/main/java/org/jcsp/solver/tree/TreeSolver.java
+++ src/main/java/org/jcsp/solver/tree/TreeSolver.java
@@ -1,25 +1,92 @@
 package org.jcsp.solver.tree;
 
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.jcsp.ConstraintSatisfactionProblem;
 import org.jcsp.assignments.Assignment;
-import org.jcsp.search.order.DefaultValueOrderer;
+import org.jcsp.consistency.arc.AC3;
+import org.jcsp.consistency.arc.Arc;
+import org.jcsp.constraints.binary.BinaryConstraint;
+import org.jcsp.search.order.DomainValuesOrderer;
+import org.jcsp.solver.Solver;
 import org.jcsp.solver.tree.selector.TreeUnassignedVariableSelector;
-import org.jcsp.solver.tree.sorter.BFSTopologicalSorter;
+import org.jcsp.solver.tree.sorter.TopologicalSorter;
+import org.jcsp.variables.Variable;
 import org.jspecify.annotations.NonNull;
 
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public interface TreeSolver {
-    default Optional<Assignment> getSolution(@NonNull ConstraintSatisfactionProblem tcsp) {
-        return getSolutions(tcsp).findFirst();
+@Slf4j
+@Value
+public class TreeSolver implements Solver {
+    @NonNull TopologicalSorter topologicalSorter;
+    @NonNull DomainValuesOrderer orderer;
+    TreeUnassignedVariableSelector.Factory selectorFactory;
+
+    @Override
+    public Stream<Assignment> getSolutions(@NonNull ConstraintSatisfactionProblem tcsp) {
+        assert tcsp.isTree();
+        var assignment = Assignment.EMPTY;
+        val rootVariableDomain = tcsp.getVariableDomains().entrySet().iterator().next();
+        val root = rootVariableDomain.getKey();
+        val X = topologicalSorter.sort(tcsp, root);
+        for (Arc arc_j : X.reversed()) {
+            val parentX_j = arc_j.getFrom();
+            val X_j = arc_j.getTo();
+            val optionalTcsp = makeArcConsistent(tcsp, parentX_j, X_j);
+            if (optionalTcsp.isPresent()) {
+                tcsp = optionalTcsp.get();
+            } else {
+                return Stream.empty();
+            }
+        }
+
+        val finalTcsp = tcsp;
+        val unassignedVariableSelector = selectorFactory.createSelector(X);
+        val domain = rootVariableDomain.getValue();
+        return domain.stream()
+                .map(value -> assignment.withValue(root, value))
+                .flatMap(rootAssignment -> populateAssignment(finalTcsp, rootAssignment, unassignedVariableSelector));
     }
 
-    Stream<Assignment> getSolutions(@NonNull ConstraintSatisfactionProblem tcsp);
+    Optional<ConstraintSatisfactionProblem> makeArcConsistent(@NonNull ConstraintSatisfactionProblem tcsp, @NonNull Variable parent, @NonNull Variable node) {
+        val allBinaryConstraints = tcsp.getAllBinaryConstraints();
+        val arcConstraints = allBinaryConstraints.stream()
+                .flatMap(binaryConstraint -> binaryConstraint.getArcs()
+                        .map(arc -> new AbstractMap.SimpleEntry<>(arc, binaryConstraint)))
+                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.mapping(Map.Entry::getValue, Collectors.toList())));
+        val arc = Arc.of(parent, node);
+        val variableDomains = new HashMap<>(tcsp.getVariableDomains());
+        for (BinaryConstraint binaryConstraint : arcConstraints.get(arc)) {
+            val optionalRevisedD = AC3.INSTANCE.revise(tcsp, arc, binaryConstraint);
+            if (optionalRevisedD.isPresent()) {
+                val revisedD = optionalRevisedD.get();
+                if (revisedD.isEmpty()) {
+                    log.warn("Domain of variable {} is empty after MAKE-ARC-CONSISTENT({}, {})", arc.getTo(), arc.getFrom(), arc.getTo());
+                    return Optional.empty();
+                }
+                variableDomains.put(parent, revisedD);
+            }
+        }
+        return Optional.of(tcsp.toBuilder().clearVariableDomains().variableDomains(variableDomains).build());
+    }
 
-    interface Factory {
-        Factory INSTANCE = () -> new TreeSolverImpl(BFSTopologicalSorter.INSTANCE, DefaultValueOrderer.INSTANCE, TreeUnassignedVariableSelector.Factory.INSTANCE);
-
-        TreeSolver createTreeSolver();
+    Stream<Assignment> populateAssignment(@NonNull ConstraintSatisfactionProblem tcsp, @NonNull Assignment assignment, @NonNull TreeUnassignedVariableSelector selector) {
+        log.debug("Searching tree with assignment: {}", assignment);
+        if (assignment.isComplete(tcsp)) {
+            log.info("Found tree solution {}", assignment);
+            return Stream.of(assignment);
+        }
+        val variable = selector.select(tcsp, assignment);
+        return orderer.order(tcsp, variable, assignment)
+                .map(value -> assignment.withValue(variable, value))
+                .filter(next -> next.isConsistent(tcsp))
+                .flatMap(next -> populateAssignment(tcsp, next, selector));
     }
 }
--- src/test/java/org/jcsp/solver/tree/TreeSolverTest.java
+++ src/test/java/org/jcsp/solver/tree/TreeSolverTest.java
@@ -1,12 +1,87 @@
 package org.jcsp.solver.tree;
 
+import lombok.val;
+import org.jcsp.ConstraintSatisfactionProblem;
+import org.jcsp.assignments.Assignment;
+import org.jcsp.constraints.binary.BinaryNotEqualsConstraint;
+import org.jcsp.domains.Domain;
+import org.jcsp.domains.EnumDomain;
+import org.jcsp.search.order.DefaultValueOrderer;
+import org.jcsp.solver.tree.selector.TreeUnassignedVariableSelector;
+import org.jcsp.solver.tree.sorter.BFSTopologicalSorter;
 import org.junit.jupiter.api.Test;
 
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.jcsp.solver.AustraliaMapColouringTest.Colour.GREEN;
+import static org.jcsp.solver.AustraliaMapColouringTest.Colour.RED;
+import static org.jcsp.solver.AustraliaMapColouringTest.NSW;
+import static org.jcsp.solver.AustraliaMapColouringTest.NT;
+import static org.jcsp.solver.AustraliaMapColouringTest.Q;
+import static org.jcsp.solver.AustraliaMapColouringTest.V;
+import static org.jcsp.solver.AustraliaMapColouringTest.WA;
 
 public class TreeSolverTest {
+
+    public static Domain DOMAIN = new EnumDomain(EnumSet.of(RED, GREEN));
+    public static Domain DOMAIN_RED_ONLY = new EnumDomain(EnumSet.of(RED));
+    ConstraintSatisfactionProblem australiaWithoutSA = ConstraintSatisfactionProblem.builder()
+            .variableDomain(WA, DOMAIN)
+            .variableDomain(NT, DOMAIN)
+            .variableDomain(Q, DOMAIN)
+            .variableDomain(NSW, DOMAIN)
+            .variableDomain(V, DOMAIN)
+            .constraint(BinaryNotEqualsConstraint.builder().left(WA).right(NT).build())
+            .constraint(BinaryNotEqualsConstraint.builder().left(NT).right(Q).build())
+            .constraint(BinaryNotEqualsConstraint.builder().left(Q).right(NSW).build())
+            .constraint(BinaryNotEqualsConstraint.builder().left(NSW).right(V).build())
+            .build();
+    TreeSolver treeSolver = new TreeSolver(BFSTopologicalSorter.INSTANCE, DefaultValueOrderer.INSTANCE, TreeUnassignedVariableSelector.Factory.INSTANCE);
+
     @Test
-    void factory() {
-        assertThat(TreeSolver.Factory.INSTANCE.createTreeSolver()).isNotNull();
+    void getSolution() {
+        val optionalSolution = treeSolver.getSolution(australiaWithoutSA);
+        assertThat(optionalSolution).hasValueSatisfying(solution -> {
+            assertThat(solution).isIn(
+                    Assignment.of(Map.of(WA, RED, NT, GREEN, Q, RED, NSW, GREEN, V, RED)),
+                    Assignment.of(Map.of(WA, GREEN, NT, RED, Q, GREEN, NSW, RED, V, GREEN))
+            );
+        });
+    }
+
+    @Test
+    void getSolutions() {
+        assertThat(treeSolver.getSolutions(australiaWithoutSA)).hasSize(2);
+    }
+
+    @Test
+    void getSolution_inconsistent() {
+        val australiaWithoutSAAndInsufficientDomains = ConstraintSatisfactionProblem.builder()
+                .variableDomain(WA, DOMAIN_RED_ONLY)
+                .variableDomain(NT, DOMAIN_RED_ONLY)
+                .constraint(BinaryNotEqualsConstraint.builder().left(WA).right(NT).build())
+                .build();
+        assertThat(treeSolver.getSolution(australiaWithoutSAAndInsufficientDomains)).isEmpty();
+    }
+
+    @Test
+    void makeArcConsistent_revises() {
+        val problem = ConstraintSatisfactionProblem.builder()
+                .variableDomain(WA, DOMAIN)
+                .variableDomain(NT, DOMAIN_RED_ONLY)
+                .constraint(BinaryNotEqualsConstraint.builder().left(WA).right(NT).build())
+                .build();
+        assertThat(treeSolver.makeArcConsistent(problem, WA, NT).get().getDomain(WA).get().stream().toList()).isEqualTo(List.of(GREEN));
+    }
+
+    @Test
+    void getSolutions_assertIsTree() {
+        val emptyCsp = ConstraintSatisfactionProblem.builder().build();
+        assertThatThrownBy(() -> treeSolver.getSolutions(emptyCsp))
+                .isInstanceOf(AssertionError.class);
     }
 }
